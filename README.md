e7ae83eaf3d519c9567886a075b183d7252ad08b
Alysha Alix <alysha.alix@aol.com>
2025-07-03 00:36:05 -0400

    project7 update1

---
+++ README.md
@@ -0,0 +1,22 @@
+# Simple Todo App
+
+the Todo App lets the user create a check list of tasks.
+
+## Functionality
+
+This Todo App helps the user stay organized by being able to create a list of tasks needing to be accomplished and setting a date of when said tasks should be accomplished by. Tasks on the todo list are able to be checked off or deleted.
+
+## Technology
+
+HTML
+CSS
+JavaScript
+FormValidation
+Public Methods
+Private Methods
+
+## Deployment
+
+This project is deployed on GitHub Pages:
+
+- ADD LINK HERE
---
+++ components/FormValidator.js
@@ -0,0 +1,90 @@
+class FormValidator {
+    constructor(settings, formElement) {
+        this._formElement = formElement;
+        this._inputSelector = settings.inputSelector;
+        this._submitButtonSelector = settings.submitButtonSelector;
+        this._errorClass = settings.errorClass;
+        this._inputErrorClass = settings.inputErrorClass;
+        this._inactiveButtonClass = settings.inactiveButtonClass;
+    }
+
+    _showInputError(_formElement, _inputElement, errorMessage) {
+        const errorElementId = `#${_inputElement.id}-error`;
+        const errorElement = _formElement.querySelector(errorElementId);
+        _inputElement.classList.add(this._inputErrorClass);
+        errorElement.textContent = errorMessage;
+        errorElement.classList.add(this._errorClass);
+      };
+
+    _hideInputError(_formElement, _inputElement) {
+        const errorElementId = `#${_inputElement.id}-error`;
+        const errorElement = _formElement.querySelector(errorElementId);
+        _inputElement.classList.remove(this._inputErrorClass);
+        errorElement.classList.remove(this._errorClass);
+        errorElement.textContent = "";
+      };
+
+    _checkInputValidity(_inputElement) {
+        if (!_inputElement.validity.valid) {
+          this._showInputError(
+            this._formElement,
+            _inputElement,
+            _inputElement.validationMessage
+          );
+        } else {
+          this._hideInputError(this._formElement, _inputElement);
+        }
+    }
+
+    _hasInvalidInput = (inputList) => {
+        return inputList.some((inputElement) => {
+          return !inputElement.validity.valid;
+        });
+      };
+
+    _toggleButtonState() {
+        if (this._hasInvalidInput(this._inputList)) {
+            this._buttonElement.classList.add(this._inactiveButtonClass);
+            this._buttonElement.disabled = true;
+          } else {
+            this._buttonElement.classList.remove(this._inactiveButtonClass);
+            this._buttonElement.disabled = false;
+          }
+    }
+
+    _setEventListeners() {
+        this._inputList = Array.from(
+            this._formElement.querySelectorAll(this._inputSelector),
+          );
+          this._buttonElement = this._formElement.querySelector(
+            this._submitButtonSelector,
+          );
+        
+         this._toggleButtonState();
+        
+          this._inputList.forEach((inputElement) => {
+            inputElement.addEventListener("input", () => {
+              this._checkInputValidity(inputElement);
+              this._toggleButtonState();
+            });
+          });
+    }
+
+    enableValidation() {
+        this._formElement.addEventListener("submit", (evt) => {
+        evt.preventDefault();
+        });
+        this._setEventListeners();
+    };
+
+    resetValidation() {
+        this._inputList.forEach((_inputElement) => {
+            _inputElement.value = "";
+            this._hideInputError(this._formElement, _inputElement);
+        });
+        this._toggleButtonState();
+    }
+};
+
+
+export default FormValidator;
